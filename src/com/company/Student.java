ace2befb95b314428fe1d39b6b86e9b98654bf60
J.J.Ding <louis7012161@gmail.com>
2021-06-27 03:14:16 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com>
    Write down the detail of the subject
    -------------------------------------------
    finish function 1 and function 2

---
+++ src/com/company/Student.java
@@ -0,0 +1,72 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Student {
+    private int seat_number;
+    private String class_name;
+    private String name;
+    private int score;
+    Scanner sc = new Scanner(System.in);
+
+
+    // public Student(int seat_number, String class_name, String name, int score) {
+    //     this.seat_number = seat_number;
+    //     this.class_name = class_name;
+    //     this.name = name;
+    //     this.score = score;
+    // }
+
+    public int getSeat_number() {
+        return seat_number;
+    }
+
+    public String getClass_name() {
+        return class_name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    //輸入學生座號(防呆未做)
+    public void setSeat_number() {
+        System.out.println("請輸入學生座位：");
+        this.seat_number = sc.nextInt();
+    }
+
+    //輸入學生班級(防呆未做)
+    public void setClass_name() {
+        System.out.println("請輸入學生班級：");
+        this.class_name = sc.next();
+    }
+
+    //輸入學生姓名(防呆未做)
+    public void setName() {
+        System.out.println("請輸入學生姓名：");
+        this.name = sc.next();
+    }
+
+    //輸入學生成績
+    public void setScore() {
+        while (true) {
+            System.out.println("請輸入學生分數：");
+            int score = sc.nextInt();
+            if (score > 100 || score < 0) {
+                System.out.println("請輸入正確的成績!");
+            } else {
+                this.score = score;
+                break;
+            }
+        }
+
+
+    }
+
+
+}
